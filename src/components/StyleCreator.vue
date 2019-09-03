8f24d118d0224ee4998f50c38285e2f5964cfbfe
William Mayberry <williamdbmayberry@gmail.com>
2019-09-03 21:47:49 +0100

    night save night save

--- src/components/StyleCreator.vue
+++ src/components/StyleCreator.vue
@@ -2,6 +2,20 @@
   <div id="SCWrapper" style="display:grid;">
     <div @click="$emit('changeS')" v-if="state == 0" :style="{ backgroundColor: this.$store.state.config.cols.colDefault.stylrCol.tert, color:'white' }">---+ To the widget settings +---</div>
     <div id="styleCreator" v-else :style="{ backgroundColor: this.$store.state.config.cols.colDefault.stylrCol.tert, height:'100%' }">
+        <div id="boxA">other edit bits</div>
+        <div id="boxB">list options</div>
+        <div id="boxC">
+          <div id="boxH">
+            <p style="margin: 0.5em;">Select classes and ids to be styled:</p>
+            <select id="boxE" multiple>
+              <option>None</option>
+              <option value="all">All</option>
+            </select>
+          </div>
+          <div id="boxF">an example of what the output would look like (top right)</div>
+          <textarea id="boxG" placeholder="Style Code"></textarea>
+        </div>
+        <div id="boxD">list</div>
     </div>
   </div>
 </template>
@@ -26,14 +40,32 @@ export default {
   }
   #styleCreator {
     display: grid;
+    grid-template-columns: calc(50% - 5px) calc(50% - 5px);
+    grid-template-rows: calc(20% - 5px) calc(80% - 5px);
+    grid-gap: 10px;
+    padding: 10px;
   }
-  #boxA {color: red;}
-  #boxB {color: orange;}
-  #boxC {color: yellow;}
-  #boxD {color: green;}
-  #boxE {color: blue;}
-  #boxF {color: lightblue;}
-  #boxG {color: indigo;}
-  #boxH {color: violet;}
-  #boxI {color: black;}
+  #boxA {color: red; background-color: tomato;}
+  #boxB {color: orangered; background-color: orange;}
+  #boxC {color: yellow; background-color: khaki;
+    display: grid;
+    grid-template-columns: calc(50% - 5px) calc(50% - 5px);
+    grid-template-rows: calc(50% - 5px) calc(50% - 5px);
+    grid-gap: 10px;
+    padding: 10px;
+  }
+  #boxD {color: green; background-color: lime;}
+  #boxE {color: blue; background-color: aqua;
+    height: calc(100% - 2em);
+    width: 100%;
+  }
+  #boxF {color: lightblue; background-color: lightcyan;
+    margin: 20px;
+  }
+  #boxG {color: indigo; background-color: mediumpurple;
+    grid-column: 1 / span 2;
+    resize: none;
+  }
+  #boxH {color: violet; background-color: lavender;}
+  #boxI {color: black; background-color: white;}
 </style>
